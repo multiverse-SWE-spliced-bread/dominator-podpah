b97ed4a3ce46fe0f4e3c7790a2be475b7136ffcd
haroldboi <preston.harry2003@gmail.com>
2022-07-25 15:13:23 +0100

    newest

--- public/js/blockBusters.js
+++ public/js/blockBusters.js
@@ -1,2 +1,15 @@
 const boxes = document.getElementById("blocks")
-for(let i =0 ; i<)
+for(let i =0 ; i < 100;i++){
+    const box = document.createElement(`div`)
+    box.setAttribute("id",`block-${i}`)
+    box.setAttribute("class","red-block")
+    boxes.appendChild(box)
+}
+const box2 = boxes.cloneNode(true)
+box2.setAttribute('id',"block2")
+console.log(box2.childNodes)
+//for(each in box2){
+    //console.log(box2[each][0])
+    //box2[each].setAttribute("class","purple-block")
+//}
+document.body.appendChild(box2)

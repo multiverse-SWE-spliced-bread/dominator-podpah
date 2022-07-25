31862b6a721f4b905709cfa46a4bdd98c8c5ebb3
haroldboi <preston.harry2003@gmail.com>
2022-07-25 17:25:06 +0100

    ds

--- public/js/caseSwitcher.js
+++ public/js/caseSwitcher.js
@@ -20,15 +20,16 @@ sarcbtn.addEventListener("click",function(){
     let text = ""
     let go = false
     for(each in paragraph.innerText){
+        if((paragraph.innerText[each] >= 101 && paragraph.innerText[each]  <= 172)){
         if (go && paragraph.innerText[each] !== " "){
             text += paragraph.innerText[each].toUpperCase()
             go =false
-        }else if (go === false && paragraph.innerText[each] !== " "){
+        }else if (go === false && (paragraph.innerText[each] >= 101 && paragraph.innerText[each]  <= 172)){
             go =true
             text += paragraph.innerText[each].toLowerCase()
         }else{
         text += paragraph.innerText[each]}
     }
     paragraph.innerText = text
-})
+}
    

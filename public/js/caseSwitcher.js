70bbdb079d6c5e9b9b42a8da974be8217f46ac23
haroldboi <preston.harry2003@gmail.com>
2022-07-25 17:25:24 +0100

    dsad

--- public/js/caseSwitcher.js
+++ public/js/caseSwitcher.js
@@ -16,20 +16,19 @@ loudbtn.addEventListener("click",function(){
 })
 
 sarcbtn.addEventListener("click",function(){
-  
     let text = ""
     let go = false
     for(each in paragraph.innerText){
-        if((paragraph.innerText[each] >= 101 && paragraph.innerText[each]  <= 172)){
-        if (go && paragraph.innerText[each] !== " "){
+        if(paragraph.innerText[each] >= 101 && paragraph.innerText[each] <= 172){
+        if (go){
             text += paragraph.innerText[each].toUpperCase()
             go =false
-        }else if (go === false && (paragraph.innerText[each] >= 101 && paragraph.innerText[each]  <= 172)){
+        }else if (go === false){
             go =true
             text += paragraph.innerText[each].toLowerCase()
         }else{
         text += paragraph.innerText[each]}
     }
-    paragraph.innerText = text
 }
-   
+paragraph.innerText = text
+}) 
